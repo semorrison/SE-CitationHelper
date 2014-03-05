a20fca2b556166eefa218b42e2620b53b40e6e0b
Manish Goregaokar <manishsmail@gmail.com>
2014-03-05 14:51:31 +0530

    refactor.m15

--- citationhelper.user.js
+++ citationhelper.user.js
@@ -22,27 +22,20 @@ function with_jquery(f) {
 // Most code for CitationButton taken from https://github.com/Manishearth/Manish-Codes/blob/master/StackExchange/MathJaxButtonsScript.js
 
 CitationButton={
-  addGenericButton: function(text,callback,identify,pic,tooltip,force){
+  addGenericButton: function(text,callback,identify,pic,tooltip){
     //Callback must take id of textarea as argument.
-    force = typeof force !== 'undefined' ? force : false;
-    var tas=force?$('.wmd-container'):$('.wmd-container').not(".canhasbutton"+identify);
-    $.each(tas,function(){
+    $('.wmd-container').not(".hasbutton-"+identify).each(function(){
     try{
-      if($(this).find("[id^=wmd-button-row]").length==0){
-      	setTimeout(function(){CitationButton.addButton(text,callback,identify,pic,true)},100);
-      	return;
-      }else{
-      	this.className+=" canhasbutton"+identify
-      }
-      tid=$(this).find("[id^=wmd-input]")[0].id;
+      var tid=$(this).find("[id^=wmd-input]")[0].id;
       row=$(this).find("[id^=wmd-button-row]")[0];
       lastel=$(row).find(".wmd-button").not(".wmd-help-button").filter(":last");
       if(lastel.length>0){
         px=parseInt(lastel[0].style.left.replace("px",""))+25;
         //add code for background-position of span as well later
-        btn='<li class="wmd-button" style="left: '+px+'px; "><span id=hello style="background-image:url('+pic+');text-align:center;">'+text+'</span></li>';
-        $(btn).on("click",function(){callback(tid)}).attr("title",tooltip).insertAfter(lastel);
-        btn=$(row).find(".wmd-button").not(".wmd-help-button").filter(":last");
+        btn='<li class="wmd-button wmd-button-'+identify+'" style="left: '+px+'px; "><span class=citebuttonspan style="background-image:url('+pic+');text-align:center;">'+text+'</span></li>';
+        $(btn).attr("title",tooltip).insertAfter(lastel).on("click",function(e){if(e.target.tagName.toLowerCase() == "li" || e.target.tagName.toLowerCase() == "span"){callback(tid);}});
+	$(this).addClass(" hasbutton-"+identify);
+	btn=$(row).find(".wmd-button").not(".wmd-help-button").filter(":last");
         if(pic==""){
           $(btn).children('span').hover(function(){$(this).css('background','#DEEDFF')},function(){$(this).css('background','none')});
         }
@@ -51,16 +44,28 @@ CitationButton={
     })
   },
   addButton: function(){
-    CitationButton.addGenericButton('C',CitationButton.searchCallback,'cite','','hey',false)
+    StackExchange.MarkdownEditor.creationCallbacks.add(CitationButton.creationCallback)
+    CitationButton.creationCallback();
+    $(document).on('popupClose',function(){CitationSearch.currentId="";});
+    if (window.addEventListener) {
+	window.addEventListener("message", CitationSearch.listenMessage, false);
+    // TODO: Have the listener clean itself up, or have a persistent one that isn't added multiple times and knows the current textarea id / etc
+    } else {
+	window.attachEvent("onmessage", CitationSearch.listenMessage);
+    }
+  },
+  creationCallback: function(obj,prefix){
+	setTimeout(function(){
+  	CitationButton.addGenericButton('C',CitationButton.searchCallback,'cite','','Insert Citation')
+        },0)
   },
   searchCallback: function(tid){
     var selectedText=CitationButton.getSelection(tid);
-    CitationSearch.searchDialog(tid, selectedText)
+    CitationSearch.searchDialog(tid, selectedText);
   },
   getSelection: function(tid){
     try{
       ta=$('#'+tid)[0];
-      console.log(ta.value,ta.selectionStart,ta.selectionEnd)
       return ta.value.slice(ta.selectionStart,ta.selectionEnd)    
     }catch(e){
       return "";
@@ -73,37 +78,41 @@ CitationButton={
 
 CitationSearch = {
   searchDialog: function(id,selectedText){
+    if($('.popup-cite').length<0){return;}
     // Tweaked version of SE close popup code
-    var popupHTML = '<div id="popup-close-question" class="popup"><div class="popup-close"><a title="close this popup (or hit Esc)">&times;</a></div><h2 class="popup-title-container handle"> <span class="popup-breadcrumbs"></span><span class="popup-title">Insert citation</span></h2><div id="pane-main" class="popup-pane popup-active-pane" data-title="Insert Citation" data-breadcrumb="Cite"><iframe width=640 height=480 src=\'http://semorrison.github.io/citation-search/?q=$question\'/></div></div>';
+    var popupHTML = '<div id="popup-cite" class="popup"><div class="popup-close"><a title="close this popup (or hit Esc)" href="javascript:void(0)">&times;</a></div><h2 class="popup-title-container handle"> <span class="popup-breadcrumbs"></span><span class="popup-title">Insert citation</span></h2><div id="pane-main" class="popup-pane popup-active-pane" data-title="Insert Citation" data-breadcrumb="Cite"><iframe width=640 height=480 src=\'http://semorrison.github.io/citation-search/?q=$question\'/></div></div>';
     popupHTML=popupHTML.replace("$question",encodeURIComponent(selectedText));
     var blob=new Blob([popupHTML]);
     $.ajaxSetup({cache:true});
-    $('#hello').loadPopup({url:URL.createObjectURL(blob),loaded:CitationSearch.callback});
+    CitationSearch.currentId=id;
+    
+    $('#'+id).parents('.post-editor').find('.citebuttonspan')
+	     .loadPopup({url:URL.createObjectURL(blob),loaded:CitationSearch.callback})
+
     $.ajaxSetup({cache:false});
   },
   callback:function(){
-    // More or less copied from https://github.com/semorrison/citation-search/blob/gh-pages/frame-test.html
-    function listenMessage(msg) {
-			StackExchange.helpers.closePopups();
+    StackExchange.helpers.bindMovablePopups();
+
+  },
+  listenMessage: function(msg) {
+      if(StackExchange.currentId ==""){
+      	return;
+      }
+      // More or less copied from https://github.com/semorrison/citation-search/blob/gh-pages/frame-test.html
       var json = JSON.parse(msg.data);
       var cite = $('<cite>').attr('authors', json.authors)
-								  .attr('MRNumber', json.MRNumber)
-								  .attr('cite', json.cite)
-								  .append($('<a>')
-								  	.attr('href', json.url)
-								  	.append(json.title));
+			    .attr('MRNumber', json.MRNumber)
+			    .attr('cite', json.cite)
+			    .append($('<a>')
+			    .attr('href', json.url)
+			    .append(json.title));
       // TODO: Move this over to InsertCitation
-			$('#wmd-input').val($('<span></span>').append(cite).html());
-      StackExchange.MarkdownEditor.refreshAllPreviews()
-		}
-
-		if (window.addEventListener) {
-		    window.addEventListener("message", listenMessage, false);
-        // TODO: Have the listener clean itself up, or have a persistent one that isn't added multiple times and knows the current textarea id / etc
-		} else {
-		    window.attachEvent("onmessage", listenMessage);
-		}
-  }
+      $('#'+CitationSearch.currentId).val($('<span></span>').append(cite).html());
+      StackExchange.MarkdownEditor.refreshAllPreviews;
+      StackExchange.helpers.closePopups();
+    },
+    currentId: ""
 }
 
 //TODO: InsertCitation.updateEditor
