34bb0e4cbfffbf943a090f157db7d50b765e4cfb
Manish Goregaokar <manishsmail@gmail.com>
2014-03-05 15:18:30 +0530

    total.m30+comments

--- citationhelper.user.js
+++ citationhelper.user.js
@@ -17,13 +17,12 @@ function with_jquery(f) {
      document.body.appendChild(script);
 };
 
-
-
-// Most code for CitationButton taken from https://github.com/Manishearth/Manish-Codes/blob/master/StackExchange/MathJaxButtonsScript.js
+// TODO: Make (almost) everything a private method
 
 CitationButton={
   addGenericButton: function(text,callback,identify,pic,tooltip){
-    //Callback must take id of textarea as argument.
+    // Adds a generic button to all available toolbars
+    // Callback must take id of textarea as argument.
     $('.wmd-container').not(".hasbutton-"+identify).each(function(){
     try{
       var tid=$(this).find(".wmd-input")[0].id;
@@ -41,12 +40,15 @@ CitationButton={
     })
   },
   addButton: function(){
+    // Add a hook for whenever new editors are created
     StackExchange.MarkdownEditor.creationCallbacks.add(CitationButton.creationCallback)
+    // Add the button to editors which already exist
     CitationButton.creationCallback();
+    // Clear the cached id when popups are closed
     $(document).on('popupClose',function(){CitationSearch.currentId="";});
+    // Listen for incoming messages
     if (window.addEventListener) {
 	window.addEventListener("message", CitationSearch.listenMessage, false);
-    // TODO: Have the listener clean itself up, or have a persistent one that isn't added multiple times and knows the current textarea id / etc
     } else {
 	window.attachEvent("onmessage", CitationSearch.listenMessage);
     }
@@ -63,60 +65,70 @@ CitationButton={
   getSelection: function(tid){
     try{
       ta=$('#'+tid)[0];
+      // Cache selection values, they have a habit of clearing themselves
+      CitationButton.selStart=ta.selectionStart;
+      CitationButton.selEnd=ta.selectionEnd;
       return ta.value.slice(ta.selectionStart,ta.selectionEnd)    
     }catch(e){
       return "";
     }
-  }
+  },
+  selStart: 0,
+  selEnd: 0
  // TODO: Keyboard shortcuts
 }
 
-//TODO: CitationSearch.searchDialog(selectedText)
 
 CitationSearch = {
   searchDialog: function(id,selectedText){
     if($('.popup-cite').length<0){return;}
-    // Tweaked version of SE close popup code
+    // Tweaked version of SE close popup code. See popup.html for unminified HTML, genblob.sh can easily generate the below line from popup.html
     var popupHTML = '<div id="popup-cite" class="popup"><div class="popup-close"><a title="close this popup (or hit Esc)" href="javascript:void(0)">&times;</a></div><h2 class="popup-title-container handle"> <span class="popup-breadcrumbs"></span><span class="popup-title">Insert citation</span></h2><div id="pane-main" class="popup-pane popup-active-pane" data-title="Insert Citation" data-breadcrumb="Cite"><iframe width=640 height=480 src=\'http://semorrison.github.io/citation-search/?q=$question\'/></div></div>';
     popupHTML=popupHTML.replace("$question",encodeURIComponent(selectedText));
+    // Data URIs give CORS issues, but blobs are fine
     var blob=new Blob([popupHTML]);
+    // jQuery AJAX cache prevention (the addition of an _ param to the query string) breaks blob URLs.
     $.ajaxSetup({cache:true});
-    CitationSearch.currentId=id;
-    
+    CitationSearch.currentId=id; // The id is cached since I'm using a global message listener rather than turning a local listener on and off again
     $('#'+id).parents('.post-editor').find('.citebuttonspan')
 	     .loadPopup({url:URL.createObjectURL(blob),loaded:CitationSearch.callback})
-
+    // Put things back where they were
     $.ajaxSetup({cache:false});
   },
   callback:function(){
-    StackExchange.helpers.bindMovablePopups();
-
+    // Stuff to run once the popup loads. Add post-popup customization here
+    StackExchange.helpers.bindMovablePopups(); // Not totally necessary. Also doesn't work perfectly
   },
   listenMessage: function(msg) {
+      // The event handler for the message
       if(StackExchange.currentId ==""){
       	return;
       }
-      // More or less copied from https://github.com/semorrison/citation-search/blob/gh-pages/frame-test.html
-      var json = JSON.parse(msg.data);
-      var cite = $('<cite>').attr('authors', json.authors)
-			    .attr('MRNumber', json.MRNumber)
-			    .attr('cite', json.cite)
-			    .append($('<a>')
-			    .attr('href', json.url)
-			    .append(json.title));
-      // TODO: Move this over to InsertCitation
-      $('#'+CitationSearch.currentId).val($('<span></span>').append(cite).html());
-      StackExchange.MarkdownEditor.refreshAllPreviews;
+      InsertCitation.updateEditor(msg,CitationSearch.currentId);
+      StackExchange.MarkdownEditor.refreshAllPreviews();
       StackExchange.helpers.closePopups();
-    },
-    currentId: ""
+  },
+  currentId: ""
 }
 
 //TODO: InsertCitation.updateEditor
 
+InsertCitation = {
+  updateEditor: function(msg, id){
+    // More or less copied from https://github.com/semorrison/citation-search/blob/gh-pages/frame-test.html
+    var json = JSON.parse(msg.data);
+    var cite = $('<cite>').attr('authors', json.authors)
+			  .attr('MRNumber', json.MRNumber)
+			  .attr('cite', json.cite)
+			  .append($('<a>')
+			  .attr('href', json.url)
+			  .append(json.title));
+    var citeHTML=$('<span></span>').append(cite).html();
+    var val=document.getElementById(id).value;
+    document.getElementById(id).value = val.slice(0,CitationButton.selStart) + citeHTML + val.slice(CitationButton.selEnd);
+  }
+}
 
 with_jquery(function($){
-//Inject code here
-
-CitationButton.addButton('wmd-input')
+  CitationButton.addButton('wmd-input')
 });
